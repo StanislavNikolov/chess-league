a2578aba8c7fc4a365bd374468f07a9d23fc4bea
Stanislav Ch. Nikolov <stanislav.ltb@gmail.com>
2023-08-08 03:04:27 +0200

    Add recompile option with bun src/index.ts recompile

---
+++ src/compile.ts
@@ -0,0 +1,50 @@
+import { makeTmpDir } from "./utils";
+
+export async function compile(code: string) {
+  console.log("Starting compile");
+  // Copy the template project into a tmpdir.
+  const tmpdir = makeTmpDir();
+  const cp = Bun.spawn(["cp", "-r", "./bot-scaffold", tmpdir]);
+  await cp.exited;
+
+  // Fix common compilation errors.
+  code = code.replaceAll(" Timer ", "ChessChallenge.API.Timer ");
+  code = code.replaceAll(",Timer ", "ChessChallenge.API.Timer ");
+
+  // Save the code to be compiled.
+  await Bun.write(`${tmpdir}/MyBot.cs`, code);
+
+  // Run the compiler.
+  // There seems to be a bug in bun, and using the default proc.stdout, which
+  // is a readble stream, simply hangs. This is a workaround.
+  const dotnet = Bun.spawn(["dotnet", "publish", "-c", "Release"], {
+    cwd: tmpdir,
+    stdout: Bun.file(`${tmpdir}/log.txt`),
+  });
+  await dotnet.exited;
+  const stdout = await Bun.file(`${tmpdir}/log.txt`).text();
+
+  console.log("Compilation done", dotnet.exitCode);
+
+  if (dotnet.exitCode !== 0) {
+    // Delete the tmpdir.
+    const rm = Bun.spawn(["rm", "-rf", tmpdir]);
+    await rm.exited;
+    return { ok: false, msg: stdout };
+  }
+
+  const hasher = new Bun.CryptoHasher("sha256");
+  hasher.update(code);
+  const hash = hasher.digest("base64url");
+
+  await Bun.write(
+    Bun.file(`compiled/${hash}.dll`),
+    Bun.file(`${tmpdir}/bin/Release/net6.0/Chess-Challenge.dll`),
+  );
+
+  // Delete the tmpdir.
+  const rm = Bun.spawn(["rm", "-rf", tmpdir]);
+  await rm.exited;
+
+  return { ok: true, msg: "", hash };
+}
--- src/index.ts
+++ src/index.ts
@@ -4,57 +4,8 @@ import { Hono } from "hono";
 import { serveStatic } from "hono/serve-static.bun";
 
 import { Arena, makeArenaIfNeeded } from "./arena";
+import { compile } from "./compile";
 import { db } from "./db";
-import { makeTmpDir } from "./utils";
-
-async function compile(code: string) {
-  console.log("Starting compile");
-  // Copy the template project into a tmpdir.
-  const tmpdir = makeTmpDir();
-  const cp = Bun.spawn(["cp", "-r", "./bot-scaffold", tmpdir]);
-  await cp.exited;
-
-  // Fix common compilation errors.
-  code = code.replaceAll(" Timer ", "ChessChallenge.API.Timer ");
-  code = code.replaceAll(",Timer ", "ChessChallenge.API.Timer ");
-
-  // Save the code to be compiled.
-  await Bun.write(`${tmpdir}/MyBot.cs`, code);
-
-  // Run the compiler.
-  // There seems to be a bug in bun, and using the default proc.stdout, which
-  // is a readble stream, simply hangs. This is a workaround.
-  const dotnet = Bun.spawn(["dotnet", "publish", "-c", "Release"], {
-    cwd: tmpdir,
-    stdout: Bun.file(`${tmpdir}/log.txt`),
-  });
-  await dotnet.exited;
-  const stdout = await Bun.file(`${tmpdir}/log.txt`).text();
-
-  console.log("Compilation done", dotnet.exitCode);
-
-  if (dotnet.exitCode !== 0) {
-    // Delete the tmpdir.
-    const rm = Bun.spawn(["rm", "-rf", tmpdir]);
-    await rm.exited;
-    return { ok: false, msg: stdout };
-  }
-
-  const hasher = new Bun.CryptoHasher("sha256");
-  hasher.update(code);
-  const hash = hasher.digest("base64url");
-
-  await Bun.write(
-    Bun.file(`compiled/${hash}.dll`),
-    Bun.file(`${tmpdir}/bin/Release/net6.0/Chess-Challenge.dll`),
-  );
-
-  // Delete the tmpdir.
-  const rm = Bun.spawn(["rm", "-rf", tmpdir]);
-  await rm.exited;
-
-  return { ok: true, msg: "", hash };
-}
 
 function addHumanIfNotExists(name: string, email: string): number {
   try {
@@ -213,6 +164,16 @@ app.get("/api/humans/", async c => {
   return c.json(humans);
 });
 
+if (process.argv.includes('recompile')) {
+  console.log("==== Starting recompilation of all submissions ====");
+  const bots = db.query("SELECT id, name, code FROM bots").all();
+  for (const bot of bots) {
+    console.log(`Recompiling ${bot.id}:${bot.name}`);
+    await compile(bot.code);
+  }
+  process.exit(0);
+}
+
 setInterval(makeArenaIfNeeded, 1000);
 
 const port = parseInt(process.env.PORT) || 3000;
@@ -221,3 +182,4 @@ export default {
   port,
   fetch: app.fetch,
 };
+
